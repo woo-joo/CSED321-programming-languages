68b68ef72d1b7e33dea1d17900e2448fd4c24a44
woo-joo <kimuj0103@gmail.com>
2021-02-28 04:16:20 +0900

    Implement functions on integers

--- hw1/hw1.ml
+++ hw1/hw1.ml
@@ -2,11 +2,33 @@ exception Not_implemented
 
 type 'a tree = Leaf of 'a | Node of 'a tree * 'a * 'a tree
 
-let rec sum _ = raise Not_implemented
-let rec fac _ = raise Not_implemented
-let rec fib _ = raise Not_implemented
-let rec gcd _ _ = raise Not_implemented
-let rec max _ = raise Not_implemented
+let rec sum n =
+    match n with
+    | 1 -> 1
+    | _ -> n + sum (n - 1) ;;
+let rec fac n =
+    match n with
+    | 1 -> 1
+    | _ -> n * fac (n - 1) ;;
+let rec fib n =
+    match n with
+    | 0 -> 1
+    | 1 -> 1
+    | _ -> fib (n - 1) + fib (n - 2) ;;
+let rec gcd m n =
+    match m, n with
+    | 0, _ -> n
+    | _, 0 -> m
+    | _ ->
+        if m > n then gcd (m mod n) n
+        else gcd m (n mod m) ;;
+let rec max l =
+    match l with
+    | [] -> 0
+    | h :: [] -> h
+    | h :: t ->
+        if h > max t then h
+        else max t ;;
 
 let rec sum_tree _ = raise Not_implemented
 let rec depth _ = raise Not_implemented
